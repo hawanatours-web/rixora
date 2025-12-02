b48fbf13cceddb5ca4b3ff1b6a0bcdf8aaf9ea3a
hawanatours-web <hawana.tours@gmail.com>
2025-12-03 00:03:08 +0200

    Update supabaseConfig.ts

--- supabaseConfig.ts
+++ supabaseConfig.ts
@@ -4,8 +4,8 @@
 
 export const supabaseConfig = {
   // Placeholder URL to trigger demo mode
-  supabaseUrl: "https://placeholder.supabase.co", 
+  supabaseUrl: "https://qkumoqdzrzltljiazxwj.supabase.co", 
   
   // Placeholder Key
-  supabaseAnonKey: "placeholder-key" 
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFrdW1vcWR6cnpsdGxqaWF6eHdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ0MzYwMjUsImV4cCI6MjA4MDAxMjAyNX0.bLCCLn0zvunP8MNool44Yj5CQAsfToqv_psojFzATQU" 
 };
